0df445c1985d10f227c48687dbf12ebbb1af8f72
mengshangqi.123 <mengshangqi.123@bytedance.com>
2024-06-25 19:42:43 +0800

    [params] mlir demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <type_traits>
+#include <utility>
 
 #include "analysis/analysis.h"
 #include "common/log.h"
@@ -8,6 +10,8 @@
 #include "llvm/Support/raw_ostream.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
@@ -15,49 +19,26 @@
 #include "mlir/Support/FileUtilities.h"
 #include "parser/pratt_parser.h"
 #include "parser/scanner.h"
-using namespace Lox;
 
-DEFINE_string(path, "", "");
-
-// int main(int argc, char** argv) {
-//   // gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-//  // std::ifstream file(FLAGS_path);
-
-//  // std::string content((std::istreambuf_iterator<char>(file)),
-//  //                     std::istreambuf_iterator<char>());
-
-//  // Lox::Scanner scan(content);
-//  // auto tokens = scan.scanTokens();
-//  // Lox::Parser parser(*tokens.Data());
-//  // auto expr = parser.parse();
-//  // if (expr.isOk()) {
-//  //   INFO("src: {}", scan.getSource());
-//  //   INFO("{}", expr.Data()->to_string());
-//  // } else {
-//  //   ERROR("parse failed: {}", expr.ErrMsg());
-//  // }
-//  // Lox::Analysis analysis;
-//  // auto result = analysis.analysis(expr.Data());
-//  // if (not result.isOk()) {
-//  //   ERROR("analysis failed: {}", result.ErrMsg());
-//  // }
-//  //
-
-//  return 0;
-//}
 using namespace mlir;
+
+template <typename T>
+struct Destructor {
+  static void free(T* ptr) { ptr->~T(); }
+};
+
 int main(int argc, char** argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::Lox::LoxDialect>();
 
   MLIRContext ctx;
-  ctx.loadDialect<func::FuncDialect, arith::ArithDialect>();
+  ctx.loadDialect<func::FuncDialect, arith::ArithDialect, memref::MemRefDialect,
+                  scf::SCFDialect>();
 
   // 创建 OpBuilder
   OpBuilder builder(&ctx);
   auto mod = builder.create<ModuleOp>(builder.getUnknownLoc());
-
+  auto loc = builder.getUnknownLoc();
   // 设置插入点
   builder.setInsertionPointToEnd(mod.getBody());
 
@@ -78,8 +59,64 @@ int main(int argc, char** argv) {
   auto addi =
       builder.create<arith::AddIOp>(builder.getUnknownLoc(), args[0], args[1]);
 
-  // 创建 func.return
+  auto memrefType = mlir::MemRefType::get({32}, builder.getIntegerType(8));
+  auto alloc =
+      builder.create<memref::AllocaOp>(builder.getUnknownLoc(), memrefType);
+
+  mlir::func::FuncOp funcOp = mod.lookupSymbol<mlir::func::FuncOp>("test_call");
+  if (!funcOp) {
+    OpBuilder::InsertionGuard insertionGuard(builder);
+    builder.setInsertionPointToStart(mod.getBody());
+
+    funcOp = builder.create<mlir::func::FuncOp>(
+        builder.getUnknownLoc(), "test_call",
+        builder.getFunctionType({memrefType}, builder.getF32Type()));
+    funcOp.setPrivate();
+    funcOp->setAttr("llvm.linkage", builder.getStringAttr("external"));
+  }
+
+  auto ifOp = builder.create<mlir::scf::IfOp>(
+      loc, llvm::ArrayRef<mlir::Type>{builder.getF32Type()}, addi, true);
+
+  builder.setInsertionPointToStart(&ifOp.getThenRegion().front());
+  // 在这里添加你的代码，例如：
+  auto x_true = builder.create<arith::ConstantOp>(
+      loc, builder.getF32Type(), builder.getF32FloatAttr(231.2));
+
+  builder.create<mlir::scf::YieldOp>(loc, llvm::ArrayRef<mlir::Value>{x_true});
+
+  // 创建 else 分支
+  builder.setInsertionPointToStart(&ifOp.getElseRegion().front());
+  // 在这里添加你的代码，例如：
+  auto x_false = builder.create<arith::ConstantOp>(
+      loc, builder.getF32Type(), builder.getF32FloatAttr(213445.2));
+
+  builder.create<mlir::scf::YieldOp>(loc, llvm::ArrayRef<mlir::Value>{x_false});
+
+  // 将插入点设置回原来的位置
+  builder.setInsertionPointAfter(ifOp);
+
+  auto if_res = ifOp.getResult(0);
+
+  // memref::AllocOp;
+  std::vector<mlir::Value> convertedValues{alloc};
+
+  // // 创建一个调用操作。
+  auto callOp = builder.create<mlir::func::CallOp>(builder.getUnknownLoc(),
+                                                   funcOp, convertedValues);
+  auto call_res = callOp.getResult(0);
+  auto fadd = builder.create<arith::AddFOp>(loc, if_res, call_res);
+
+  auto ret = builder.create<arith::FPToSIOp>(loc, builder.getI32Type(), fadd);
+
   builder.create<func::ReturnOp>(builder.getUnknownLoc(), ValueRange({addi}));
+
   mod->print(llvm::outs());
+  INFO("str size: {}", sizeof(std::string));
+  char str_ptr[32];
+  std::string* str_val = new ((void*)str_ptr) std::string("345w345645");
+  INFO("str: {}", *str_val);
+  Destructor<std::string>::free(str_val);
+
   return 0;
 }
